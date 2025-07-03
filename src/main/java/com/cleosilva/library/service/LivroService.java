1efee3f312430765a48011b243f0284cdc6b5952
Cleo Silva <cleo_silva1906@hotmail.com>
2025-07-03 06:53:25 -0300

    feat: implement global exception handler with custom errors for book availability and user not found cases

--- src/main/java/com/cleosilva/library/service/LivroService.java
+++ src/main/java/com/cleosilva/library/service/LivroService.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 @Service
 public class LivroService {
     @Autowired
-    private LivroRepository livroRepository;
+    private  LivroRepository livroRepository;
 
 
     public Livro cadastrarLivro(Livro livro){
